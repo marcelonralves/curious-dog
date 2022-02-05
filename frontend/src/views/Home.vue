aaf10449c9285a62cfd11fc81d60ebb9db95978f
Marcelo Alves <marcelonralves@gmail.com>
2022-02-05 03:46:48 -0300

    init structure

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    Entre ou se registre!
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+}
+</script>
---
+++ frontend/src/views/Me.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="me">
+      <div class="send_message">
+          <textarea name="miau"></textarea>
+          <button type="submit">Miau</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Me'
+}
+</script>
+
+<style>
+.me{
+    background-color: #333;
+    height: 1600px;
+    color: #fff;
+    text-align: center;
+}
+</style>
